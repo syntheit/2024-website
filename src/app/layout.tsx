f11152dfab747dd51ae8b904bf1ab0acd0c4f1a4
Daniel Miller <daniel@matv.io>
2024-02-15 16:04:16 -0500

    Attempted fix for Plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
-import NavbarCustom from "~/components/Navbar/NavbarCustom";
-import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,13 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          defer
-          data-domain="matv.io"
-          src="https://analytics.matv.io/js/script.js"
-        ></script>
-      </Head>
       <body
         className={`bg-background font-sans text-foreground dark ${inter.variable}`}
       >
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import PlausibleProvider from "next-plausible";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <PlausibleProvider
+        domain="matv.io"
+        customDomain="https://analytics.matv.io"
+      >
+        {children}
+      </PlausibleProvider>
+    </NextUIProvider>
+  );
 }
