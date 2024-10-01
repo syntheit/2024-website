ad59d6c75a3cb5ccc3659b3e59b74c93baf28da5
Daniel Miller <daniel@matv.io>
2024-10-01 00:17:52 -0400

    Added Spotify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,19 @@ import {
   ResumeIcon,
   DockerIcon,
   MatrixIcon,
+  SpotifyIcon,
 } from "~/components/Icons";
 
 export default function HomePage() {
   return (
-    <main className="flex items-center justify-center w-full min-h-screen text-white bg-black">
+    <main className="flex min-h-screen w-full items-center justify-center bg-black text-white">
       {/* <div className="absolute top-0 right-0 flex gap-3 mt-10 mr-12 text-white/50">
         <h3 className="text-white/80">EN</h3>
         <h3>FR</h3>
       </div> */}
-      <div className="absolute top-0 flex flex-col items-center gap-3 mt-20">
+      <div className="absolute top-0 mt-20 flex flex-col items-center gap-3">
         <h1 className="text-4xl font-bold tracking-wide">Daniel Miller</h1>
-        <p className="tracking-wider text-tiny text-white/60">
+        <p className="text-tiny tracking-wider text-white/60">
           Software Developer & Student
         </p>
       </div>
@@ -37,16 +38,16 @@ export default function HomePage() {
           href="/about"
         >
           <CardHeader className="absolute top-1 z-10 flex-col !items-start">
-            <p className="font-bold uppercase text-tiny text-white/60 drop-shadow-2xl">
+            <p className="text-tiny font-bold uppercase text-white/60 drop-shadow-2xl">
               Learn about me
             </p>
-            <h4 className="font-medium text-white text-large drop-shadow-2xl">
+            <h4 className="text-large font-medium text-white drop-shadow-2xl">
               Bio
             </h4>
           </CardHeader>
           <Image
             removeWrapper
-            className="z-0 object-cover w-full h-full"
+            className="z-0 h-full w-full object-cover"
             src="/images/indian-falls-overlook.webp"
           />
         </Card>
@@ -57,16 +58,16 @@ export default function HomePage() {
           href="/projects&work"
         >
           <CardHeader className="absolute top-1 z-10 flex-col !items-start">
-            <p className="font-bold uppercase text-tiny text-white/60">
+            <p className="text-tiny font-bold uppercase text-white/60">
               Projects & Work
             </p>
-            <h4 className="font-medium text-white text-large">
+            <h4 className="text-large font-medium text-white">
               See what I've done
             </h4>
           </CardHeader>
           <Image
             removeWrapper
-            className="z-0 object-cover w-full h-full"
+            className="z-0 h-full w-full object-cover"
             src="/images/thinkpad.webp"
           />
         </Card>
@@ -77,16 +78,16 @@ export default function HomePage() {
           href="https://wordsbydaniel.xyz"
         >
           <CardHeader className="absolute top-1 z-10 flex-col !items-start">
-            <p className="font-bold uppercase text-tiny text-white/60 drop-shadow-2xl">
+            <p className="text-tiny font-bold uppercase text-white/60 drop-shadow-2xl">
               wordsbydaniel.xyz
             </p>
-            <h4 className="font-medium text-white text-large drop-shadow-2xl">
+            <h4 className="text-large font-medium text-white drop-shadow-2xl">
               Read about things I like
             </h4>
           </CardHeader>
           <Image
             removeWrapper
-            className="z-0 object-cover w-full h-full"
+            className="z-0 h-full w-full object-cover"
             src="/images/new-orleans-streetcar.webp"
           />
         </Card>
@@ -97,8 +98,8 @@ export default function HomePage() {
           as={Link}
           href="https://photos.matv.io/share/SjsOTA6_x2G_KhXaMnFewvKfwjq93td7WMyB1wd_xp2LhYP2ppHchTzobv8QtGsfR9A"
         >
-          <CardHeader className="absolute z-10 flex-col items-start top-1">
-            <p className="font-bold uppercase text-tiny text-white/60 drop-shadow-2xl">
+          <CardHeader className="absolute top-1 z-10 flex-col items-start">
+            <p className="text-tiny font-bold uppercase text-white/60 drop-shadow-2xl">
               The world
             </p>
             <h4 className="text-2xl font-medium text-white drop-shadow-2xl">
@@ -107,7 +108,7 @@ export default function HomePage() {
           </CardHeader>
           <Image
             removeWrapper
-            className="z-0 object-cover w-full h-full scale-125 -translate-y-6"
+            className="z-0 h-full w-full -translate-y-6 scale-125 object-cover"
             src="/images/waffle-house.webp"
           />
         </Card>
@@ -115,8 +116,8 @@ export default function HomePage() {
           isFooterBlurred
           className="col-span-12 h-[300px] w-full sm:col-span-7"
         >
-          <CardHeader className="absolute z-10 flex-col items-start top-1">
-            <p className="font-bold uppercase text-tiny text-white/60 drop-shadow-2xl">
+          <CardHeader className="absolute top-1 z-10 flex-col items-start">
+            <p className="text-tiny font-bold uppercase text-white/60 drop-shadow-2xl">
               Get in touch
             </p>
             <h4 className="text-xl font-medium text-white/90 drop-shadow-2xl">
@@ -125,11 +126,11 @@ export default function HomePage() {
           </CardHeader>
           <Image
             removeWrapper
-            className="z-0 object-cover w-full h-full"
+            className="z-0 h-full w-full object-cover"
             src="/images/quebec-street.webp"
           />
           <CardFooter className="absolute bottom-0 z-10 border-t-1 border-default-600 bg-black/40 dark:border-default-100">
-            <div className="flex items-center flex-grow gap-2">
+            <div className="flex flex-grow items-center gap-2">
               <Button
                 isIconOnly
                 aria-label="LinkedIn"
@@ -162,6 +163,19 @@ export default function HomePage() {
                   href="https://matrix.to/#/@syntheit:matrix.org"
                 >
                   <MatrixIcon size={24} />
+                </Button>
+              </Tooltip>
+              <Tooltip
+                content="https://open.spotify.com/user/312k3mbad43po7ghp67ralyw2j6q"
+                size="md"
+              >
+                <Button
+                  isIconOnly
+                  aria-label="Spotify"
+                  as={Link}
+                  href="https://open.spotify.com/user/312k3mbad43po7ghp67ralyw2j6q"
+                >
+                  <SpotifyIcon size={24} />
                 </Button>
               </Tooltip>
               <Tooltip content="daniel@matv.io" size="md">
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -160,3 +160,29 @@ export const MatrixIcon = ({
     <path d="M17 15v-3.5a2.5 2.5 0 1 0 -5 0v.5" />
   </svg>
 );
+
+export const SpotifyIcon = ({
+  size = 24,
+  width,
+  height,
+  ...props
+}: IconSvgProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    height={size || height}
+    width={size || width}
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+    <path d="M8 11.973c2.5 -1.473 5.5 -.973 7.5 .527" />
+    <path d="M9 15c1.5 -1 4 -1 5 .5" />
+    <path d="M7 9c2 -1 6 -2 10 .5" />
+  </svg>
+);
